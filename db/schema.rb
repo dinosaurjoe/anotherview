5622b6f719a4a57204f66ad977b9cd76497ffefb
Joe Schafer <joeschaferastula@gmail.com>
2017-08-22 11:55:10 -0500

    trying to fix user creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170819195324) do
+ActiveRecord::Schema.define(version: 20170822155932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 20170819195324) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "salt"
+    t.string   "encrypted_password"
   end
 
 end
